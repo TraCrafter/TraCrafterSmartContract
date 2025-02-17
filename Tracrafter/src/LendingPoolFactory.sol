693c1ac021c375b7705ab7c79ee103e4c34793e8
Ghoza <ghoza60@gmail.com>
2025-02-18 00:33:29 +0700

    Update LendingPoolFactory.sol

--- Tracrafter/src/LendingPoolFactory.sol
+++ Tracrafter/src/LendingPoolFactory.sol
@@ -11,8 +11,15 @@ contract LendingPoolFactory {
         address creator, address lendingPool, address token1, address token2, address oracle, uint256 LTV
     );
 
-    address public oracle = 0x6E87c01682E547Bf69c73B5F0A1b4aAAE91A1EE1;
+    struct Pools {
+        address collateralToken;
+        address borrowToken;
+        address lendingPoolAddress;
+    }
 
+    address public oracle = 0x6E87c01682E547Bf69c73B5F0A1b4aAAE91A1EE1;
+    uint256 public id;
+    mapping(uint256 => Pools) public lendingPoolId;
     mapping(address => address) public lendingPoolCollects;
 
     function createLendingPool(address LendingPoolToken1, address LendingPoolToken2, uint256 LTV)
@@ -21,6 +28,8 @@ contract LendingPoolFactory {
     {
         // if (lendingPoolCollects[LendingPoolToken1] == LendingPoolToken2) revert lendingPoolHasCreated();
         LendingPool lendingPool = new LendingPool(LendingPoolToken1, LendingPoolToken2, oracle, LTV);
+        ++id;
+        lendingPoolId[id] = Pools(LendingPoolToken1, LendingPoolToken2, address(lendingPool));
         lendingPoolCollects[LendingPoolToken1] = LendingPoolToken2;
         emit CreateLendingPool(msg.sender, address(lendingPool), LendingPoolToken1, LendingPoolToken2, oracle, LTV);
         return address(lendingPool);
